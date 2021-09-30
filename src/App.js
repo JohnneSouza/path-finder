9dbbc85fd1fcbd927c61f9d4b9a86648594f2c5d
Johnnes Souza <jhonnesf5@gmail.com>
2021-09-30 15:41:01 -0300

    Set rows and cols manualy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useState } from "react";
+// import { useState } from "react";
 import PathFinder from "./components/PathFinder";
 
 function App() {
 
-  const [rows, setRows] = useState(8);
-  const [cols, setCols] = useState(20);
+  // const [rows, setRows] = useState(8);
+  // const [cols, setCols] = useState(20);
   
 
   return (
     <>
       {/* <input type="number" onChange={setRows}></input> */}
       {/* <input type="number" onChange={setCols}></input> */}
-      <PathFinder rows={rows} columns={cols}></PathFinder>
+      <PathFinder rows={8} columns={20}></PathFinder>
     </>
   );
 }
